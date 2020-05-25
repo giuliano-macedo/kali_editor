fc9ffc0b81d3b713a1bdb552c3b9a8243292ab51
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2020-05-25 12:46:55 -0400

    implementing positions list

--- lib/core/strokes_file_stream.dart
+++ lib/core/strokes_file_stream.dart
@@ -18,14 +18,17 @@ class _Event {
 
 class StrokesFileStream {
   RandomAccessFile stream;
-  StreamController<_Event> queue;
-  Queue<Stroke> _cache;
+  StreamController<_Event> opQueue;
+  Queue<int> _positions;
   StrokesFileStream(String fileName) {
     stream = File(
       fileName,
-    ).openSync(mode: FileMode.write);
-    //initialize cache
-    //whenever there is a event i the queue, execute its operation
+    ).openSync(mode: FileMode.append);
+    if (stream.lengthSync() == 0)
+      stream.writeStringSync(Stroke.header + "\n");
+    else
+      _parsePositions();
+    //whenever there is a event in the opQueue, execute its operation
     _queueStream().listen((_Event event) {
       switch (event.op) {
         case _Operation.write:
@@ -39,36 +42,54 @@ class StrokesFileStream {
       }
     });
   }
+
+  void _parsePositions([buffsize = 4096]) {
+    //get every string \n position, so that it can be properly deleted
+    // it will set position to 0 initially, then to final
+    final List<int> buff = List<int>(buffsize);
+    final List<int> ans = [];
+    int pos = 0, bytesRead;
+    stream.setPositionSync(0);
+    do {
+      bytesRead = stream.readIntoSync(buff);
+      for (int i = 0; i < buff.length; i++) {
+        if (buff[i] == "\n".codeUnitAt(0)) {
+          ans.add(pos + i);
+        }
+      }
+      pos += bytesRead;
+    } while (bytesRead != 0);
+    for (int p in ans.getRange(1, ans.length)) _positions.add(p);
+  }
+
   Stream<_Event> _queueStream() async* {
-    await for (var event in queue.stream) {
+    await for (var event in opQueue.stream) {
       if (event.op == _Operation.stop) break;
       yield event;
     }
+    stream.closeSync();
   }
 
   void _write(Stroke stroke) {
+    _positions.add(stream.positionSync());
     stream.writeStringSync(stroke.toCSV());
-    _cache.add(stroke);
   }
 
   void _delete() {
-    //TODO:PROBLEM
-    if (_cache.isEmpty) throw Exception("cache is empty");
-    int lineLength = _cache.removeFirst().toCSV().length;
-    stream.setPositionSync(lineLength);
-    // stream.writeByteSync(-1);
+    if (_positions.isEmpty) throw Exception("cache is empty");
+    stream.truncateSync(_positions.removeFirst());
   }
 
   void add(Stroke stroke) {
-    queue.add(_Event(_Operation.write, stroke));
+    opQueue.add(_Event(_Operation.write, stroke));
   }
 
   void remove(int n) {
     assert(n > 0);
-    for (int i = 0; i < n; i++) queue.add(_Event(_Operation.delete, null));
+    for (int i = 0; i < n; i++) opQueue.add(_Event(_Operation.delete, null));
   }
 
   void close() {
-    queue.add(_Event(_Operation.stop, null));
+    opQueue.add(_Event(_Operation.stop, null));
   }
 }
