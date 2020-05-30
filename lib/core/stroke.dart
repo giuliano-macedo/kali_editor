43e24158e92993c2a45a21179686f6e9bb188192
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2020-05-30 13:05:42 -0400

    better internal representation

--- lib/core/stroke.dart
+++ lib/core/stroke.dart
@@ -1,13 +1,24 @@
+import 'dart:ui';
+
 class Stroke {
-  static const header="xn,yn,xn+1,yn+1,pen_up_pen_down";
-  int xn;
-  int yn;
-  int xnPlus;
-  int ynPlus;
+  static const header = "xn,yn,xn+1,yn+1,pen_up_pen_down";
   int penup;
 
-  Stroke(this.xn, this.yn, this.xnPlus, this.ynPlus, this.penup);
-  String toCSV(){
+  Offset origin;
+  Offset destination;
+
+  Stroke(xn, yn, xnPlus, ynPlus, this.penup) {
+    origin = Offset(xn, yn);
+    destination = Offset(xnPlus, ynPlus);
+  }
+  Stroke.fromOffset(this.origin, this.destination, this.penup);
+
+  get xn => origin.dx;
+  get yn => origin.dy;
+  get xnPlus => destination.dx;
+  get ynPlus => destination.dy;
+
+  String toCSV() {
     return "${this.xn},${this.yn},${this.xnPlus},${this.ynPlus},${this.penup}";
   }
 }
