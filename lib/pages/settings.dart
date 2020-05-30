1a41a5721e00a7103423d9144b1dc6c02963a94f
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2020-05-30 11:48:57 -0400

    read version from pubspec.yaml

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import "package:flutter/material.dart";
+import 'package:yaml/yaml.dart';
 
 import 'new_project.dart';
 
@@ -10,6 +13,15 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   bool isDarkMode = false; //TODO:Change to provider
   String projectPath = "/mnt/storage/todo";
+  Map appConfig;
+
+  @override
+  void initState() {
+    File("../../pubspec.yaml")
+        .readAsString()
+        .then((String content) => appConfig = loadYaml(content));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +60,7 @@ class _SettingsPageState extends State<SettingsPage> {
                 onTap: () => showAboutDialog(
                   context: context,
                   applicationName: "Kali Editor",
-                  applicationVersion: "0.0.1",
+                  applicationVersion: appConfig["version"],
                   applicationIcon: null, //TODO
                   applicationLegalese:
                       "A tool to generate online handwriting sequences in any language, based on some list of senteces",
