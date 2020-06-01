3c6e1a149ec0784648c03c80a8ccc2c71bd698bd
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2020-06-01 13:34:08 -0400

    file_picker from field

---
+++ lib/widgets/file_picker_form_field.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+
+class FilePickerFormField extends StatelessWidget {
+  final Function(String) validator;
+  final Function(String) onSaved;
+  final String labelText;
+  final String hintText;
+  final List<String> allowedExtensions;
+  const FilePickerFormField(
+      {Key key,
+      this.validator,
+      this.onSaved,
+      this.labelText,
+      this.hintText,
+      this.allowedExtensions})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FormField<String>(
+      initialValue: "",
+      validator: validator,
+      onSaved: onSaved,
+      builder: (FormFieldState<String> state) => InkWell(
+        onTap: () => FilePicker.getFile(
+          allowedExtensions: allowedExtensions,
+          type: FileType.custom,
+        ).then((File f) {
+          if (f == null) return;
+          state.didChange(f.path);
+          state.save();
+        }),
+        child: InputDecorator(
+          decoration: InputDecoration(
+            icon: Icon(Icons.folder),
+            labelText: state.value.isNotEmpty ? labelText : hintText,
+          ),
+          isEmpty: state.value.isEmpty,
+          child: state.value.isEmpty ? Container() : Text(state.value),
+        ),
+      ),
+    );
+  }
+}
